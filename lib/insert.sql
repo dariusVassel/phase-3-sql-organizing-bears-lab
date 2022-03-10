4835601c77ea482e7d6376e5d213c7bb45614d97
+ <+>
2022-03-10 15:20:45 -0500

    Complete

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'Brown', 'Calm', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'F', 'Brown', 'Wild', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 2, 'F', 'Black', 'Wild', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 9, 'M', 'Brown', 'Wise', False);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'Brown', 'Wild', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 7, 'M', 'Brown', 'Wild', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 5, 'F', 'Brown', 'Calm', True);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 5, 'F', 'Brown', 'Wild', True);
